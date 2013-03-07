1e83de63c9f563ae2207e4fa26448e5050f9a466
Drew Harry <drew.harry@gmail.com>
2013-03-07 10:31:12 -0500

    Added a bit that deploys the jquery ui library in the deploy folder, too, if present.

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,6 +5,8 @@
 
 scp -i ~/Documents/multitude/infrastructure/ec2/multitude.pem index.html ubuntu@jabber.multitudecorp.com:/usr/share/nginx/www/.
 
+scp -i ~/Documents/multitude/infrastructure/ec2/multitude.pem *.js ubuntu@jabber.multitudecorp.com:/usr/share/nginx/www/.
+
 scp -r -i ~/Documents/multitude/infrastructure/ec2/multitude.pem ../res ubuntu@jabber.multitudecorp.com:/usr/share/nginx/www/.
 
 scp -r -i ~/Documents/multitude/infrastructure/ec2/multitude.pem ../src ubuntu@jabber.multitudecorp.com:/usr/share/nginx/www/.
